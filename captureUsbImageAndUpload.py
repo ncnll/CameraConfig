cf9c223a967fba3505638c9e693aa124db9e8b86
ncnll <yincharm@qq.com>
2015-05-31 16:33:16 +0000

    upload demo has been done

---
+++ captureUsbImageAndUpload.py
@@ -0,0 +1,42 @@
+import pygame
+import pygame.camera
+import pygame.image
+from pygame.locals import *
+from poster.encode import multipart_encode
+from poster.streaminghttp import register_openers
+
+import urllib2
+
+pygame.init()
+pygame.camera.init()
+
+########Get cpu serial number
+def getserial():
+	cpuseiral = "0000000000000000"
+	try:
+		cpufile = open('/proc/cpuinfo','r')
+		for line in cpufile:
+			if line[0:6]=='Serial':
+				cpuserial=line[10:26]
+		cpufile.close()	
+	except:
+		cpuserial="Error00000000000"
+	return cpuserial
+
+
+cam = pygame.camera.Camera("/dev/video0",(1920, 1080))
+cam.start()
+image=cam.get_image()
+pygame.image.save(image, "image.jpg")
+
+register_openers()
+
+with open("image.jpg", 'r') as f:
+	cpuserial = getserial()
+	datagen, headers = multipart_encode({"file":f,"cpuId":cpuserial,"index":"0","productItemId":"5565d304b09f8dd00cca2ff0"})
+	request = urllib2.Request("http://192.168.1.106:8091/index/uploadCameraPhoto",datagen, headers)
+	response = urllib2.urlopen(request)
+	print response.info()
+	print response.geturl()
+	print response.getcode()
+	print response.read()
