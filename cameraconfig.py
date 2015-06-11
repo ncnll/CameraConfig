afbc521127563e4b2f8ccdcb6811157e5979269b
charmyin <976582265@qq.com>
2015-06-11 14:54:42 +0000

    save to database successfully

--- cameraconfig.py
+++ cameraconfig.py
@@ -7,7 +7,7 @@ import json
 import sqlite3
 
 #check whether /home/pi/database/uploadConfig.db is exists, if not, create a new one
-directory_path = '/home/pi/database/uploadconfig'
+directory_path = '/home/pi/database/uploadconfig/'
 uploadConfig_file_path=directory_path+'uploadConfig.db'
 if not os.path.exists(directory_path):
 	os.makedirs(directory_path)
@@ -22,11 +22,15 @@ row1 = c.fetchone()
 if row1 is not None :
 	print row1[0]
 else :
-	c.execute('''CREATE TABLE CAMERACONFIG(cameraId text, cameraType integer, localShootInterval integer, uploadShootInterval integer, workingTime text, localResolution text, remoteResolution text, updateTime text, startDate text, endDate text, uploadPath text)''')
+	c.execute('''CREATE TABLE CAMERACONFIG(serialNumber text, cameraType integer, localShootInterval integer, uploadShootInterval integer, workingTime text, localResolution text, remoteResolution text, updateTime text, startDate text, endDate text, uploadPath text)''')
 	print 'table is not exists'
 
 #Load config properties from server
 stdout = listdir("/home/pi/v4l/by-id/")
+##Delete all camera not exists
+#conn.execute("DELETE FROM CAMERACONFIG")
+#conn.commit()
+
 for line in stdout:
 	usbserial=line[4:22]
 	#print usbserial
@@ -41,9 +45,16 @@ for line in stdout:
 	responseString = response.read()
 	responseJson = json.loads(responseString)
 	if len(responseJson['rows'])>0:
-		print responseJson['rows'][0]['startDate']
-	#Save to database
-	#c.execute('SELECT * FROM CAMERACONFIG WHERE cameraId=?', t)
-	#print c.fetchone()
-
-#print stdout
+		
+		print responseJson['rows'][0]['serialNumber']
+		#Save to database
+		c=conn.execute('SELECT * FROM CAMERACONFIG WHERE serialNumber=?', (responseJson['rows'][0]['serialNumber'],))
+		configRow=c.fetchone()
+		insertSql = "INSERT INTO CAMERACONFIG VALUES %s" % ((str(responseJson['rows'][0]['serialNumber']), format(responseJson['rows'][0]['cameraType'], 'x'), format(responseJson['rows'][0]['localShootInterval'], 'x'),format(responseJson['rows'][0]['uploadShootInterval'], 'x'), str(responseJson['rows'][0]['workingTime']), str(responseJson['rows'][0]['localResolution']), str(responseJson['rows'][0]['remoteResolution']), str(responseJson['rows'][0]['updateTime']), str(responseJson['rows'][0]['startDate']), str(responseJson['rows'][0]['endDate']), str(responseJson['rows'][0]['uploadPath'])),)
+		print insertSql
+		if configRow is not None:
+			print configRow
+		else :
+			#print str(insertSql)
+			conn.execute(insertSql)
+			conn.commit()
