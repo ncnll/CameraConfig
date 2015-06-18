00ba2887389bab7d7246983fc09883593793bbfd
charmyin <976582265@qq.com>
2015-06-18 14:07:26 +0800

    Time demo has been done ~

--- test.py
+++ test.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python
 import time
-from dateutil.parser import parse
+
 current_milli_time = lambda: int(round(time.time() * 1000))
 print current_milli_time()
 strtest = "usb-046d_0825_99999990-video-index2"
-print strtest[-1:]
+#print strtest[-1:]
+#print current_milli_time()
 
-print current_milli_time()
-
-
+epochTimeStr = time.gmtime(time.time())
+print epochTimeStr
 timeStr = "2015-06-07T16:10:41.519Z"
-
-d = parse(timeStr)
-print d
+isoTime = time.strptime(timeStr,'%Y-%m-%dT%H:%M:%S.%fZ')
+print isoTime
